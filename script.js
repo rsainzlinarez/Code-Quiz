4d7bcb7225a4f9c7e1c77a951542fa2ea576f95f
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-11-29 19:57:29 -0800

    Added List of questions

---
+++ script.js
@@ -0,0 +1,36 @@
+var questions = [
+    {
+question1: "Commonuly used data types DO NOT include:",
+choices: ['strings', 'booleans', 'alerts', 'numbers'],
+answer: 'alerts',
+    },
+       {
+question2: "The condition in an if/ese is enclosed within__________.",
+choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+answer: 'alerts',
+    },
+       {
+question3: "Arrays in JavaScript can be used to store __________.",
+choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+answer: 'all of the above',
+    },
+       {
+question4: "String values must be recorded within __________when being assigned to variables.",
+choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+answer: 'quotes',
+    },
+       {
+question5: "A very useful tool during development and debugging for printing content to the debugger is:",
+choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+answer: 'console.log',
+    },
+     
+];
+
+
+
+  
+  
+ 
+ 
+
