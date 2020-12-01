3e605d5fbf9689c03c426f31f2d1fa0e695d5c99
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-11-30 20:53:44 -0800

    Connected choice buttons to array data

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-var questions = [
+var questionsList = [
     {
-question1: "Commonuly used data types DO NOT include:",
+question: "Commonuly used data types DO NOT include:",
 choices: ['strings', 'booleans', 'alerts', 'numbers'],
 answer: 'alerts',
     },
        {
-question2: "The condition in an if/ese is enclosed within__________.",
+question: "The condition in an if/ese is enclosed within__________.",
 choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
 answer: 'alerts',
     },
        {
-question3: "Arrays in JavaScript can be used to store __________.",
+question: "Arrays in JavaScript can be used to store __________.",
 choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
 answer: 'all of the above',
     },
        {
-question4: "String values must be recorded within __________when being assigned to variables.",
+question: "String values must be recorded within __________when being assigned to variables.",
 choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
 answer: 'quotes',
     },
        {
-question5: "A very useful tool during development and debugging for printing content to the debugger is:",
+question: "A very useful tool during development and debugging for printing content to the debugger is:",
 choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
 answer: 'console.log',
     },
      
 ];
 
- 
+ //List of all variables required from index.html file
 var i = 0;
-var questionsSection = document.querySelector('startWindow');
+var questionsSection = document.querySelector('#startWindow');
 var timerEl = document.querySelector('#timer');
 var startButtonEl = document.querySelector('#startButton');
 var scoreRecordedEl = document.querySelector('#scoresLink');
+var titleEl = document.querySelector('#title');
+var instructionsEl = document.querySelector('#instructions');
+var answerButtonA = document.querySelector('.choiceA');
+var answerButtonB = document.querySelector('.choiceB');
+var answerButtonC = document.querySelector('.choiceC');
+var answerButtonD = document.querySelector('.choiceD');
 
 
+
+// List of variables needed for the questions section
+// var questionsSectionEL = document.querySelector('.questionsSectionEl');
+// var answerA = querySelector('.a');
+// var answerB = querySelector('.b');
+// var answerC = querySelector('.c');
+// var answerD = querySelector('.d');
+
+// Counter starts when start button is clicked
 startButtonEl.addEventListener('click', function () {
     var timeLeft = 2;
     var timeInterval = setInterval(function() {
@@ -50,6 +65,21 @@ startButtonEl.addEventListener('click', function () {
     
 
     });
+// First questions appears when counter is clicked
+    startButtonEl.addEventListener('click', function(){
+       
+            titleEl.innerHTML= (questionsList[0].question);
+            
+            answerButtonA.innerHTML= (questionsList[0].choices[0]);
+            answerButtonB.innerHTML= (questionsList[0].choices[1]);
+            answerButtonC.innerHTML= (questionsList[0].choices[1]);
+            answerButtonD.innerHTML= (questionsList[0].choices[1]);
+         
+            
+            instructionsEl.innerHTML = "";
+        
+    // titleEl.innerHTML ="hello";
 
-    
+   
+    });
 
