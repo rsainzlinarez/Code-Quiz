b33a9fe6bd6ec7bfdd89f8f45a4382ea23880176
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-12-02 10:13:35 -0800

    Addes list of questions

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 var questionsList = [
     {
-question: "Commonuly used data types DO NOT include:",
+title: "Commonuly used data types DO NOT include:",
 choices: ['strings', 'booleans', 'alerts', 'numbers'],
 answer: 'alerts',
     },
        {
-question: "The condition in an if/ese is enclosed within__________.",
+title: "The condition in an if/ese is enclosed within__________.",
 choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-answer: 'alerts',
+answer: 'curly brackets',
     },
        {
-question: "Arrays in JavaScript can be used to store __________.",
+title: "Arrays in JavaScript can be used to store __________.",
 choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
 answer: 'all of the above',
     },
        {
-question: "String values must be recorded within __________when being assigned to variables.",
+title: "String values must be recorded within __________when being assigned to variables.",
 choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
 answer: 'quotes',
     },
        {
-question: "A very useful tool during development and debugging for printing content to the debugger is:",
+title: "A very useful tool during development and debugging for printing content to the debugger is:",
 choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
 answer: 'console.log',
     },
@@ -35,59 +35,111 @@ var startButtonEl = document.querySelector('#startButton');
 var scoreRecordedEl = document.querySelector('#scoresLink');
 var titleEl = document.querySelector('#title');
 var instructionsEl = document.querySelector('#instructions');
-var answerButtonA = document.querySelector('.choiceA');
+var answerButtonA= document.querySelector(".choiceA");
 var answerButtonB = document.querySelector('.choiceB');
 var answerButtonC = document.querySelector('.choiceC');
 var answerButtonD = document.querySelector('.choiceD');
 
 
-
-// List of variables needed for the questions section
-// var questionsSectionEL = document.querySelector('.questionsSectionEl');
-// var answerA = querySelector('.a');
-// var answerB = querySelector('.b');
-// var answerC = querySelector('.c');
-// var answerD = querySelector('.d');
+var timer = 60;
+var score = 0;
+var currentQuestion = 0;
 
 // Counter starts when start button is clicked
 startButtonEl.addEventListener('click', function () {
-    var timeLeft = 2;
-    var timeInterval = setInterval(function() {
-        timerEl.textContent = timeLeft + " seconds remaining";
-        timeLeft--;
     
-        if (timeLeft === -1) {
+    var timeInterval = setInterval(function() {
+        timerEl.textContent = timer + " seconds remaining";
+        timer--;
+    
+        if (timer === -1) {
             clearInterval(timeInterval);
             
         }
     
       }, 1000);
-    
+    nextQuestion();
 
     });
-// When start button is clicked, start button disappear and choice answers appear
-    startButtonEl.addEventListener('click', function(){
-       
+
+
+
+      startButtonEl.addEventListener('click', function(){
+        // Startbutton disappears
         startButtonEl.style.display = 'none';
 
+        // Instructions disappear
+        instructionsEl.style.display = 'none';
+
+        // Buttos that contain the answer choices appear
         answerButtonA.style.display = 'block'; 
         answerButtonB.style.display = 'block'; 
         answerButtonC.style.display = 'block'; 
         answerButtonD.style.display = 'block'; 
 
+        ;
 
+
+     
+});
+// First Questions appears
+startButtonEl.addEventListener('click', function(){
+    titleEl.innerHTML= (questionsList[0].title);
+
+    answerButtonA.innerHTML= (questionsList[0].choices[0]);
+    answerButtonB.innerHTML= (questionsList[0].choices[1]);
+    answerButtonC.innerHTML= (questionsList[0].choices[2]);
+    answerButtonD.innerHTML= (questionsList[0].choices[3]);
+
+   
+   
+});
+// Second question appears
+answerButtonC.addEventListener('click', function(){
+    titleEl.innerHTML= (questionsList[1].title);
+
+    answerButtonA.innerHTML = (questionsList[1].choices[0]);
+    answerButtonB.innerHTML = (questionsList[1].choices[1]);
+    answerButtonC.innerHTML = (questionsList[1].choices[2]);
+    answerButtonD.innerHTML = (questionsList[1].choices[3]);
+
+   
+   
+});
+answerButtonD.addEventListener('click', function(){
+    titleEl.innerHTML= (questionsList[2].title);
+
+    answerButtonA.innerHTML = (questionsList[2].choices[0]);
+    answerButtonB.innerHTML = (questionsList[2].choices[1]);
+    answerButtonC.innerHTML = (questionsList[2].choices[2]);
+    answerButtonD.innerHTML = (questionsList[2].choices[3]);
+
+   
+   
 });
 
-// First questions appears when counter is clicked
-    startButtonEl.addEventListener('click', function(){
-       
-            titleEl.innerHTML= (questionsList[0].question);
-            
-            answerButtonA.innerHTML= (questionsList[0].choices[0]);
-            answerButtonB.innerHTML= (questionsList[0].choices[1]);
-            answerButtonC.innerHTML= (questionsList[0].choices[1]);
-            answerButtonD.innerHTML= (questionsList[0].choices[1]);
+answerButtonA.addEventListener('click', function(){
+    titleEl.innerHTML= (questionsList[3].title);
+
+    answerButtonA.innerHTML = (questionsList[3].choices[0]);
+    answerButtonB.innerHTML = (questionsList[3].choices[1]);
+    answerButtonC.innerHTML = (questionsList[3].choices[2]);
+    answerButtonD.innerHTML = (questionsList[3].choices[3]);
+
    
-    });
-    
+   
+});
+answerButtonB.addEventListener('click', function(){
+    titleEl.innerHTML= (questionsList[4].title);
+
+    answerButtonA.innerHTML = (questionsList[4].choices[0]);
+    answerButtonB.innerHTML = (questionsList[4].choices[1]);
+    answerButtonC.innerHTML = (questionsList[4].choices[2]);
+    answerButtonD.innerHTML = (questionsList[4].choices[3]);
+
+   
+   
+});
+
+
 
